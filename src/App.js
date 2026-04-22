b523596261c342627195ff6cdeade404713767e0
unknown <meet37269@gmail.com>
2026-04-22 11:50:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data/PeientData.json";
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    gender: "",
+    phone: "",
+    email: "",
+    address: "",
+    bloodGroup: ""
+  });
+ const [members, setMembers] = useState(data.members); 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Patient Data:", formData);
+    alert("Patient Registered Successfully!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h2>Patient Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="firstName" placeholder="First Name" onChange={handleChange} required /><br />
+        <input name="lastName" placeholder="Last Name" onChange={handleChange} required /><br />
+
+        <select name="gender" onChange={handleChange} required>
+          <option value="">Select Gender</option>
+          <option>Male</option>
+          <option>Female</option>
+        </select><br />
+        <input type="date" name="dob" onChange={handleChange} required /><br />
+        <input name="phone" placeholder="Phone" onChange={handleChange} required /><br />
+        <input name="email" placeholder="Email" onChange={handleChange} /><br /><br />
+        <textarea name="address" placeholder="Address" onChange={handleChange}></textarea><br />
+
+        <select name="bloodGroup" onChange={handleChange}>
+          <option value="">Blood Group</option>
+          <option>A+</option>
+          <option>B+</option>
+          <option>O+</option>
+          <option>AB+</option>
+        </select><br />
+
+        <button type="submit">Register</button>
+      </form>
+      
     </div>
   );
 }
 
-export default App;
+export default App; 
